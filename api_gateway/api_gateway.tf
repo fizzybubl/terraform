ff6c1edc7606a4253486798d679c09c69b0e2528
Dragos Sandica <daniil865dragos@gmail.com>
2024-08-03 11:42:18 +0300

    added sqs integration; todo: add integration response and method response

--- api_gateway/api_gateway.tf
+++ api_gateway/api_gateway.tf
@@ -1,32 +1,60 @@
-resource "aws_api_gateway_rest_api" "example" {
- name = "SQS API GW"
- description = "Gateway fronting an SQS queue for secure internet exposure"
+resource "aws_api_gateway_rest_api" "sqs_access" {
+  name        = "SQS API GW"
+  description = "Gateway fronting an SQS queue for secure internet exposure"
 }
 
 
-resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_api_gateway_resource" "sqs_resource" {
+  rest_api_id = aws_api_gateway_rest_api.sqs_access.id
+  parent_id   = aws_api_gateway_rest_api.sqs_access.root_resource_id
+  path_part   = "{foo}"
 }
 
 
-resource "aws_api_gateway_stage" "example" {
-  deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  stage_name    = "example"
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = aws_api_gateway_rest_api.sqs_access.id
+  resource_id   = aws_api_gateway_resource.sqs_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
 }
+
 
 
 resource "aws_api_gateway_integration" "sqs_integration" {
-    type = "AWS"
-    resource_id = aws_api_gateway_rest_api.example.id
-    http_method = "POST"
-    rest_api_id = aws_api_gateway_rest_api.example.id
+  type                    = "AWS"
+  resource_id             = aws_api_gateway_resource.sqs_resource.id
+  http_method             = aws_api_gateway_method.post.http_method
+  integration_http_method = "POST"
+  rest_api_id             = aws_api_gateway_rest_api.sqs_access.id
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  uri                     = "arn:aws:apigateway:${var.region}:sqs:path/${aws_sqs_queue.terraform_queue.name}"
+  credentials = aws_iam_role.sqs_access_role.arn
+
+  request_parameters = {
+    "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
+  }
+
+  request_templates = {
+    "application/json" = "Action=SendMessage&MessageBody=$input.body"
+  }
 }
+
+
+# resource "aws_api_gateway_deployment" "sqs_deployment" {
+#   rest_api_id = aws_api_gateway_rest_api.example.id
+
+#   triggers = {
+#     redeployment = sha1(jsonencode(aws_api_gateway_stage.v1.stage_name))
+#   }
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+
+# resource "aws_api_gateway_stage" "v1" {
+#   deployment_id = aws_api_gateway_deployment.sqs_deployment.id
+#   rest_api_id   = aws_api_gateway_rest_api.sqs_access.id
+#   stage_name    = "v1"
+# }
--- api_gateway/iam.tf
+++ api_gateway/iam.tf
@@ -1,9 +1,30 @@
 resource "aws_iam_role" "sqs_access_role" {
   name = "AWSGatewayRoleForSQS"
 
+  assume_role_policy = <<EOT
+  {
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : ["apigateway.amazonaws.com"]
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
+    ]
+  }
+  EOT
+}
+
+resource "aws_iam_role_policy" "sqs_access_policy" {
+  name = "SQSAccessPolicy"
+  role = aws_iam_role.sqs_access_role.id
+
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
+  policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
@@ -15,8 +36,4 @@ resource "aws_iam_role" "sqs_access_role" {
       }
     ]
   })
-
-  tags = {
-    tag-key = "tag-value"
-  }
 }
