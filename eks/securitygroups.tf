c1d74c3e1b49b71cd5b3336b511b3c121f114982
= <=>
2024-09-30 08:52:56 +0300

    fixes for sg control plane

--- eks/securitygroups.tf
+++ eks/securitygroups.tf
@@ -1,11 +1,25 @@
+locals {
+  common_sg_tags = {
+    "kubernetes.io/cluster/${aws_eks_cluster.control_plane.id}" = "owned"
+    "aws:eks:cluster-name" = aws_eks_cluster.control_plane.id
+    "Name" = "eks-cluster-sg-${aws_eks_cluster.control_plane.id}"
+  }
+
+  control_plange_required_egress = [{port = 443, protocol = "tcp"}, {port = 53, protocol = "tcp"}, {port = 53, protocol = "udp"}, {port = 10250, protocol = "tcp"}]
+}
+
 resource "aws_security_group" "control_plane" {
   name   = "control_plane_sg"
   vpc_id = aws_vpc.custom_vpc.id
+
+  tags = local.common_sg_tags
 }
 
 resource "aws_security_group" "worker_nodes" {
   name   = "worker_nodes_sg"
   vpc_id = aws_vpc.custom_vpc.id
+
+  tags = local.common_sg_tags
 }
 
 resource "aws_vpc_security_group_ingress_rule" "worker_nodes_access" {
@@ -13,7 +27,7 @@ resource "aws_vpc_security_group_ingress_rule" "worker_nodes_access" {
   ip_protocol                  = "tcp"
   from_port                    = 1
   to_port                      = 65535
-  referenced_security_group_id = aws_security_group.worker_nodes.id
+  cidr_ipv4 = aws_vpc.custom_vpc.cidr_block
 }
 
 
@@ -31,23 +45,24 @@ resource "aws_vpc_security_group_ingress_rule" "control_plane_access" {
   ip_protocol                  = "tcp"
   from_port                    = 1
   to_port                      = 65535
+  cidr_ipv4 = aws_vpc.custom_vpc.cidr_block
+}
+
+
+# resource "aws_vpc_security_group_egress_rule" "internet_access" {
+#   security_group_id = aws_security_group.worker_nodes.id
+#   ip_protocol       = "tcp"
+#   from_port         = 1
+#   to_port           = 65535
+#   cidr_ipv4         = "0.0.0.0/0"
+# }
+
+
+resource "aws_vpc_security_group_egress_rule" "control_plane_egress" {
+  count = length(local.control_plange_required_egress)
+  security_group_id = aws_security_group.control_plane.id
+  ip_protocol       = local.control_plange_required_egress[count.index].protocol
+  from_port         = local.control_plange_required_egress[count.index].port
+  to_port           = local.control_plange_required_egress[count.index].port
   referenced_security_group_id = aws_security_group.control_plane.id
-}
-
-
-resource "aws_vpc_security_group_ingress_rule" "vpc_access" {
-  security_group_id = aws_security_group.worker_nodes.id
-  ip_protocol       = "tcp"
-  from_port         = 1
-  to_port           = 65535
-  cidr_ipv4         = aws_vpc.custom_vpc.cidr_block
-}
-
-
-resource "aws_vpc_security_group_egress_rule" "internet_access" {
-  security_group_id = aws_security_group.worker_nodes.id
-  ip_protocol       = "tcp"
-  from_port         = 1
-  to_port           = 65535
-  cidr_ipv4         = "0.0.0.0/0"
 }
