0c55eca22083f68ad69bd95176e50f5ca22ea9a4
= <=>
2024-10-16 10:43:57 +0300

    added vpc endpoints for private cluster

--- eks/nodegroups.tf
+++ eks/nodegroups.tf
@@ -9,7 +9,7 @@ resource "aws_eks_node_group" "worker_nodes" {
     min_size     = 2
   }
 
-  subnet_ids    = aws_subnet.public_subnet[*].id
+  subnet_ids    = aws_subnet.private_subnet[*].id
   node_role_arn = aws_iam_role.worker.arn
 
   launch_template {
