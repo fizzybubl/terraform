c63a29f8010a27aa7fc0f456015c1b2ef5339524
fizzybubl <99640183+fizzybubl@users.noreply.github.com>
2024-11-26 15:02:40 +0200

    started working on routing policies

--- aws/route53/route53.tf
+++ aws/route53/route53.tf
@@ -16,6 +16,34 @@ resource "aws_route53_record" "record" {
   type     = each.value.type
   ttl      = each.value.ttl
   records  = each.value.records
+
+  cidr_routing_policy {
+    
+  }
+
+  failover_routing_policy {
+
+  }
+
+  geolocation_routing_policy {
+
+  }
+
+  geoproximity_routing_policy {
+
+  }
+
+  latency_routing_policy {
+
+  }
+
+  multivalue_answer_routing_policy {
+
+  }
+
+  weighted_routing_policy {
+    
+  }
 }
 
 
