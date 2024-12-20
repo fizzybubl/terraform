f7611ad599b384c2f15a0a116923e7fd8077ad4a
= <=>
2024-12-20 12:30:49 +0200

    added deny all inbound nsg and allow ssh nsg

--- azure/virtual_network/virtual_network.tf
+++ azure/virtual_network/virtual_network.tf
@@ -58,3 +58,59 @@ resource "azurerm_nat_gateway_public_ip_association" "example" {
   nat_gateway_id       = azurerm_nat_gateway.nat.id
   public_ip_address_id = azurerm_public_ip.static_public.id
 }
+
+
+resource "azurerm_network_security_group" "allow_ssh" {
+  name                = "test-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "22"
+  destination_port_range      = "22"
+  source_address_prefix       = "0.0.0.0/0"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.allow_ssh.name
+}
+
+
+resource "azurerm_network_security_group" "deny_all" {
+  name                = "test-nsg-deny-all"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_network_security_rule" "deny_all" {
+  name                        = "deny-all"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "Tcp"
+  source_port_range           = "22"
+  destination_port_range      = "22"
+  source_address_prefix       = "0.0.0.0/0"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.deny_all.name
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "public" {
+  subnet_id                 = azurerm_subnet.public_subnet.id
+  network_security_group_id = azurerm_network_security_group.allow_ssh.id
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  subnet_id                 = azurerm_subnet.private_subnet.id
+  network_security_group_id = azurerm_network_security_group.deny_all.id
+}
