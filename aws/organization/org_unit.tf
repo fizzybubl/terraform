4644451096f74491e58fd4edd07d3c60d91ed197
= <=>
2025-02-01 14:35:37 +0200

    first

---
+++ aws/organization/org_unit.tf
@@ -0,0 +1,24 @@
+resource "aws_organizations_organizational_unit" "dev" {
+  name      = "DEV"
+  parent_id = data.aws_organizations_organization.root_org.roots[0].id
+
+  tags = {
+    "Created by"="AcousticBear"
+  }
+  tags_all = {
+    "Created by"="AcousticBear"
+  }
+}
+
+
+resource "aws_organizations_organizational_unit" "prod" {
+  name      = "PROD"
+  parent_id = data.aws_organizations_organization.root_org.roots[0].id
+
+  tags = {
+    "Created by" = "AcousticBear"
+  }
+  tags_all = {
+    "Created by" = "AcousticBear"
+  }
+}
