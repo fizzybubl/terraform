36f990671fcf12e1e89ff73902c33bb8d56742e3
= <=>
2025-02-10 22:08:21 +0200

    added managed_directory and simple_directory

--- aws/microsoft_managed_directory/microsoft_managed_directory.tf
+++ aws/microsoft_managed_directory/microsoft_managed_directory.tf
@@ -12,6 +12,19 @@ resource "aws_directory_service_directory" "managed_ad" {
 }
 
 
+resource "aws_directory_service_directory" "simple_ad" {
+  count    = var.directory_type == "SimpleAD" ? 1 : 0
+  name     = "corp.example.com"
+  password = var.admin_password
+  size     = "Small"
+
+  vpc_settings {
+    vpc_id     = ""
+    subnet_ids = []
+  }
+}
+
+
 resource "aws_directory_service_directory" "ad_connector" {
   count    = var.directory_type == "ADConnector" ? 1 : 0
   name     = "corp.example.com"
