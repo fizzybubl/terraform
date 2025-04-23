17192a0639361c6b98a08ef57915728978a424b9
fizzybubl <99640183+fizzybubl@users.noreply.github.com>
2025-04-23 11:28:02 +0300

    added output for static website endpoints

---
+++ aws/s3_replication_cross_account/outputs.tf
@@ -0,0 +1,8 @@
+output "dev_endpoint" {
+    value = aws_s3_bucket_website_configuration.dev.website_endpoint
+}
+
+
+output "prod_endpoint" {
+    value = aws_s3_bucket_website_configuration.prod.website_endpoint
+}
