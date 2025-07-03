36b16edf4227fa4b7b0fcd93bf81d85fd51c65df
= <=>
2025-07-03 18:11:00 +0300

    added sg

--- aws/rds/firewall.tf
+++ aws/rds/firewall.tf
@@ -1,6 +1,24 @@
 module "name" {
-  source = "../ec2/modules/security_groups"
-  vpc_id = module.cloud_vpc.vpc_id
-  name = "rds"
+  source      = "../ec2/modules/security_groups"
+  vpc_id      = module.cloud_vpc.vpc_id
+  name        = "rds"
   description = "rdss"
+
+  ingress_rules = {
+    "instance" = {
+      from_port = "3306"
+      to_port   = "3306"
+      protocol  = "tcp"
+      cidr_ipv4 = module.cloud_vpc.cidr_block
+    }
+  }
+
+  egress_rules = {
+    "all" = {
+      from_port = 1
+      to_port   = 65535
+      protocol  = "tcp"
+      cidr_ipv4 = module.cloud_vpc.cidr_block
+    }
+  }
 }
--- aws/rds/rds.tf
+++ aws/rds/rds.tf
@@ -2,7 +2,7 @@ module "rds" {
   source                 = "./modules/rds"
   username               = "admin"
   password               = "admin"
-  subnet_ids             = [for az in local.az_ids: module.cloud_db[az]]
+  subnet_ids             = [for az in local.az_ids : module.cloud_db[az]]
   db_name                = "test"
   vpc_security_group_ids = [""]
 }
