b7097fe5919b504bc019966d27f6b25a52720dcc
fizzybubl <99640183+fizzybubl@users.noreply.github.com>
2025-08-08 10:47:03 +0300

    Update route53_hosted_zone.tf

--- aws/route53/hosted_zone/route53_hosted_zone.tf
+++ aws/route53/hosted_zone/route53_hosted_zone.tf
@@ -1,3 +1,12 @@
-resource "aws_route53_zone" "main" {
-  name  = var.zone_name
+resource "aws_route53_zone" "this" {
+  name    = var.zone_name
+  comment = var.zone_description
+
+  dynamic "vpc" {
+    for_each = var.vpc_ids
+
+    content {
+      vpc_id = each.value
+    }
+  }
 }
