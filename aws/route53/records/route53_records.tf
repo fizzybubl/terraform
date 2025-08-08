2e54c47db1fb5f6b568fbfed2ab130445abd5b12
fizzybubl <99640183+fizzybubl@users.noreply.github.com>
2025-08-08 10:02:29 +0300

    Fix r53 record dynamic blocks

--- aws/route53/records/route53_records.tf
+++ aws/route53/records/route53_records.tf
@@ -1,63 +1,68 @@
-resource "aws_route53_record" "record" {
-  for_each = var.record_data
-  zone_id  = each.value.zone_id
-  name     = each.key
-  type     = each.value.type
-  ttl      = each.value.ttl
-  records  = each.value.records
+resource "aws_route53_record" "this" {
+  for_each       = var.record_data
+  zone_id        = each.value.zone_id
+  name           = each.key
+  type           = each.value.type
+  ttl            = each.value.ttl
+  records        = each.value.records
   set_identifier = each.value.set_identifier
 
   dynamic "failover_routing_policy" {
-    for_each = each.value.failover_routing_policy != null ? ["failover_routing_policy"] : []
+    iterator = frp
+    for_each = each.value.failover_routing_policy != null ? [each.value.failover_routing_policy] : []
     content {
-      type = each.value.failover_routing_policy.type
+      type = frp.value.type
     }
   }
 
   dynamic "geolocation_routing_policy" {
-    for_each = each.value.geolocation_routing_policy != null ? ["geolocation_routing_policy"] : []
+    iterator = grp
+    for_each = each.value.geolocation_routing_policy != null ? [each.value.geolocation_routing_policy] : []
     content {
-      continent   = each.value.geolocation_routing_policy.continent
-      country     = each.value.geolocation_routing_policy.country
-      subdivision = each.value.geolocation_routing_policy.subdivision
+      continent   = grp.value.continent
+      country     = grp.value.country
+      subdivision = grp.value.subdivision
     }
   }
 
   dynamic "geoproximity_routing_policy" {
-    for_each = each.value.geoproximity_routing_policy != null ? ["geoproximity_routing_policy"] : []
+    iterator = gpxrp
+    for_each = each.value.geoproximity_routing_policy != null ? [each.value.geoproximity_routing_policy] : []
     content {
-      aws_region = each.value.geoproximity_routing_policy.aws_region
-      bias       = each.value.geoproximity_routing_policy.bias
+      aws_region = gpxrp.value.aws_region
+      bias       = gpxrp.value.bias
       coordinates {
-        latitude  = each.value.geoproximity_routing_policy.coordinates.latitude
-        longitude = each.value.geoproximity_routing_policy.coordinates.longitude
+        latitude  = gpxrp.value.coordinates.latitude
+        longitude = gpxrp.value.coordinates.longitude
       }
-      local_zone_group = each.value.geoproximity_routing_policy.local_zone_group
+      local_zone_group = gpxrp.value.local_zone_group
     }
   }
 
   dynamic "latency_routing_policy" {
-    for_each = each.value.latency_routing_policy != null ? ["latency_routing_policy"] : []
+    iterator = lrp
+    for_each = each.value.latency_routing_policy != null ? [each.value.latency_routing_policy] : []
     content {
-      region = each.value.latency_routing_policy.region
+      region = lrp.value.region
     }
   }
 
   dynamic "weighted_routing_policy" {
-    for_each = each.value.weighted_routing_policy != null ? ["weighted_routing_policy"] : []
+    iterator = wrp
+    for_each = each.value.weighted_routing_policy != null ? [each.value.weighted_routing_policy] : []
     content {
-      weight = each.value.weighted_routing_policy.weight
+      weight = wrp.value.weight
     }
   }
 
   multivalue_answer_routing_policy = each.value.multivalue_answer_routing_policy
 
   dynamic "alias" {
-    for_each = each.value.alias != null ? ["alias"] : []
+    for_each = each.value.alias != null ? [each.value.alias] : []
     content {
-      name                   = each.value.alias.name
-      zone_id                = each.value.alias.zone_id
-      evaluate_target_health = each.value.alias.evaluate_target_health
+      name                   = alias.value.name
+      zone_id                = alias.value.zone_id
+      evaluate_target_health = alias.value.evaluate_target_health
     }
   }
 }
