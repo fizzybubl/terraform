b234178add6d9745cad39c8462e0713b7a182f0d
fizzybubl <99640183+fizzybubl@users.noreply.github.com>
2025-08-11 11:43:03 +0300

    Update variables.tf

--- aws/route53/hosted_zone/variables.tf
+++ aws/route53/hosted_zone/variables.tf
@@ -18,24 +18,33 @@ variable "vpc_ids" {
 
 variable "record_data" {
   type = map(object({
-    name                             = string
-    zone_id                          = optional(string, null)
-    ttl                              = optional(string, null)
-    type                             = optional(string, null)
-    records                          = optional(list(string), null)
-    failover_routing_policy          = optional(object({ type = string }), null)
-    geolocation_routing_policy       = optional(object({ continent = string, country = string, subdivision = string }), null)
-    geoproximity_routing_policy      = optional(object({ aws_region = string, bias = number, coordinates = object({ latitude = string, longitude = string }), local_zone_group = string }), null)
-    latency_routing_policy           = optional(object({ region = string }), null)
-    weighted_routing_policy          = optional(object({ weight = number }), null)
-    multivalue_answer_routing_policy = optional(string, null)
-    health_check_id                  = optional(string, null)
+    name                    = string
+    zone_id                 = optional(string)
+    ttl                     = optional(string)
+    type                    = optional(string)
+    records                 = optional(list(string))
+    failover_routing_policy = optional(object({ type = string }))
+    geolocation_routing_policy = optional(object({
+      continent   = optional(string),
+      country     = optional(string),
+      subdivision = optional(string)
+    }))
+    geoproximity_routing_policy = optional(object({
+      aws_region       = optional(string),
+      bias             = optional(number),
+      coordinates      = optional(object({ latitude = string, longitude = string })),
+      local_zone_group = optional(string)
+    }))
+    latency_routing_policy           = optional(object({ region = string }))
+    weighted_routing_policy          = optional(object({ weight = number }))
+    multivalue_answer_routing_policy = optional(string)
+    health_check_id                  = optional(string)
     set_identifier                   = string
     alias = optional(object({
       name                   = string
       zone_id                = string
-      evaluate_target_health = bool
-    }), null)
+      evaluate_target_health = optional(bool, true)
+    }))
   }))
   default = {}
 }
