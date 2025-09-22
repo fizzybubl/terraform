fb89aeb3d0dc336d40b81cfdbfcf790b5e3f8938
Dragos <daniil865dragos@gmail.com>
2025-09-22 16:41:58 +0300

    fix

--- aws/multi_region_internal_alb/r53_new/import.tf
+++ aws/multi_region_internal_alb/r53_new/import.tf
@@ -1,6 +1,6 @@
 # R53 ZONES -> IMPORT THE FRA ASSOCIATED ZONE
 import {
-  id = ""
+  id = "Z06868462EY7KNUGLVKF9"
   to = module.fra.aws_route53_zone.this[0]
 }
 
@@ -8,11 +8,11 @@ import {
 # R53 Records
 
 import {
-  id = ""
+  id = "Z06868462EY7KNUGLVKF9_internal.example.org_A_eu-central-1"
   to = module.fra.aws_route53_record.this["fra"]
 }
 
 import {
-  id = "" 
+  id = "Z06868462EY7KNUGLVKF9_internal.example.org_A_eu-west-1"
   to = module.fra.aws_route53_record.this["dub"]
 }
