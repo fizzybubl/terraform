28df3d86821f2517b11247f3a7c3df6469a49517
Dragos <daniil865dragos@gmail.com>
2025-10-10 17:39:52 +0300

    progress

--- aws/eks/modules/eks/addons.tf
+++ aws/eks/modules/eks/addons.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_addon" "this" {
   for_each                 = var.addons
   cluster_name             = aws_eks_cluster.this.name
-  addon_name               = each.value.addon_name
+  addon_name               = each.key
   addon_version            = each.value.addon_version
   service_account_role_arn = each.value.role_arn
   dynamic "pod_identity_association" {
